a04472389a08d224c8f3480c082dc0c81b04bc4f
Curry TPH <currytph@gmail.com>
2025-02-19 22:04:08 -0600

    fix

--- main.cpp
+++ main.cpp
@@ -17,35 +17,42 @@
 #define ID_GENERATE 1
 #define ID_SAVE 2
 #define ID_EXIT 3
+#define ID_PARAGRAPHS 4
+#define ID_SENTENCES 5
+#define ID_WORDS 6
 
 std::mutex mtx;
 std::vector<std::wstring> wordList = { L"Lorem", L"ipsum", L"dolor", L"sit", L"amet", L"consectetur", L"adipiscing", L"elit" };
 std::mt19937 rng(std::random_device{}());
-HWND hwndTextBox, hwndButtonGenerate, hwndButtonSave;
+HWND hwndTextBox, hwndButtonGenerate, hwndButtonSave, hwndParagraphs, hwndSentences, hwndWords;
 
-// Convert `std::string` to `std::wstring` (for wide-char compatibility)
-std::wstring stringToWString(const std::string& str) {
-    int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.length(), NULL, 0);
-    std::wstring wstrTo(size_needed, 0);
-    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.length(), &wstrTo[0], size_needed);
-    return wstrTo;
-}
-
-std::wstring generateText(size_t numSentences, size_t wordsPerSentence) {
+std::wstring generateText(size_t numParagraphs, size_t numSentences, size_t wordsPerSentence) {
     std::wostringstream text;
     std::uniform_int_distribution<size_t> wordDist(0, wordList.size() - 1);
-    for (size_t i = 0; i < numSentences; ++i) {
-        std::lock_guard<std::mutex> lock(mtx);
-        for (size_t j = 0; j < wordsPerSentence; ++j) {
-            text << wordList[wordDist(rng)] << L" ";
+    for (size_t p = 0; p < numParagraphs; ++p) {
+        for (size_t i = 0; i < numSentences; ++i) {
+            std::lock_guard<std::mutex> lock(mtx);
+            for (size_t j = 0; j < wordsPerSentence; ++j) {
+                text << wordList[wordDist(rng)] << L" ";
+            }
+            text << L". ";
         }
-        text << L". ";
+        text << L"\n\n";
     }
     return text.str();
 }
 
 void onGenerate() {
-    auto futureText = std::async(std::launch::async, generateText, 10, 8);
+    wchar_t bufferP[10], bufferS[10], bufferW[10];
+    GetWindowText(hwndParagraphs, bufferP, 10);
+    GetWindowText(hwndSentences, bufferS, 10);
+    GetWindowText(hwndWords, bufferW, 10);
+
+    size_t numParagraphs = _wtoi(bufferP);
+    size_t numSentences = _wtoi(bufferS);
+    size_t wordsPerSentence = _wtoi(bufferW);
+
+    auto futureText = std::async(std::launch::async, generateText, numParagraphs, numSentences, wordsPerSentence);
     std::wstring generatedText = futureText.get();
     SetWindowText(hwndTextBox, generatedText.c_str());
 }
@@ -78,9 +85,15 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
         hwndTextBox = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_MULTILINE | ES_AUTOVSCROLL,
             10, 10, 460, 200, hwnd, NULL, NULL, NULL);
         hwndButtonGenerate = CreateWindowW(L"BUTTON", L"Generate", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-            10, 220, 100, 30, hwnd, (HMENU)ID_GENERATE, NULL, NULL);
+            10, 250, 100, 30, hwnd, (HMENU)ID_GENERATE, NULL, NULL);
         hwndButtonSave = CreateWindowW(L"BUTTON", L"Save", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-            120, 220, 100, 30, hwnd, (HMENU)ID_SAVE, NULL, NULL);
+            120, 250, 100, 30, hwnd, (HMENU)ID_SAVE, NULL, NULL);
+        hwndParagraphs = CreateWindowW(L"EDIT", L"1", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER,
+            10, 220, 50, 20, hwnd, (HMENU)ID_PARAGRAPHS, NULL, NULL);
+        hwndSentences = CreateWindowW(L"EDIT", L"5", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER,
+            70, 220, 50, 20, hwnd, (HMENU)ID_SENTENCES, NULL, NULL);
+        hwndWords = CreateWindowW(L"EDIT", L"8", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER,
+            130, 220, 50, 20, hwnd, (HMENU)ID_WORDS, NULL, NULL);
         break;
     case WM_DESTROY:
         PostQuitMessage(0);
@@ -98,7 +111,7 @@ int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE hPrev, LPWSTR lpCmd, int nCmdShow
     wc.lpszClassName = L"TextGeneratorApp";
     RegisterClass(&wc);
     HWND hwnd = CreateWindowW(L"TextGeneratorApp", L"Text Generator", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-        100, 100, 500, 300, NULL, NULL, hInst, NULL);
+        100, 100, 500, 320, NULL, NULL, hInst, NULL);
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
